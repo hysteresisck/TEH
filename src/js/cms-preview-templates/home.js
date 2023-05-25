43bdaf4a4b0b5c8ae932c4fdd4260a442ad54b2f
Jaycoder7 <68451905+Jaycoder7@users.noreply.github.com>
2023-05-25 09:10:44 -0400

    linkedIn page link read more

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -57,7 +57,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="{{.buttonLink}}" className="btn raise">Read more</a>
+              <a href="{{https://www.linkedin.com/company/the-entrepreneurial-hour/?viewAsMember=true}}" className="btn raise">Read more</a>
             </div>
 
           </div>
